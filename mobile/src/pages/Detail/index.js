961498385de4f66e5bffd7f1747c7a72b3c08087
Alfredo Savi <alfredosavi@hotmail.com>
2020-04-01 21:39:58 -0300

    Incidents mobile

---
+++ mobile/src/pages/Detail/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { View } from "react-native";
+
+// import logoImg from "../../assets/logo.png";
+
+function Detail() {
+  return <View />;
+}
+export default Detail;
