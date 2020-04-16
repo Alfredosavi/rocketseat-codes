146cfa8d1244d288aebd35c016c936cead3d1f0b
Alfredo Savi <alfredosavi@hotmail.com>
2020-04-16 03:24:43 -0300

    Mail notification

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '6782e5e4a351c2',
+    pass: 'dc2258f0a6f85f',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
