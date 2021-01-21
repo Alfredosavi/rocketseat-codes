8e9ff05010b0caba08c4bf82de6c197307d4e77f
Alfredo Savi <alfredosavi@hotmail.com>
2021-01-21 15:06:28 -0300

    DTO

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepositories';
+
 import AuthUserService from '@modules/users/services/AuthUserService';
 
 const sessionsRouter = Router();
@@ -6,7 +8,8 @@ const sessionsRouter = Router();
 sessionsRouter.post('/', async (request, response) => {
   const { email, password } = request.body;
 
-  const authUser = new AuthUserService();
+  const usersRepository = new UsersRepository();
+  const authUser = new AuthUserService(usersRepository);
 
   const { user, token } = await authUser.execute({
     email,
