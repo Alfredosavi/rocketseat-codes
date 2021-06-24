5157086386655dec88f4e2a3604296898221c8c9
Alfredo Savi <alfredosavi@hotmail.com>
2021-06-24 08:19:38 -0300

    feat: create database and create user

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import "reflect-metadata";
+
+import express from "express";
+import { router } from "./routes";
+
+import "./database";
+
+const app = express();
+
+app.use(express.json());
+app.use(router);
+
+app.listen(process.env.PORT || 3000, () =>
+  console.log(`Server is running ${process.env.PORT || 3000}`)
+);
