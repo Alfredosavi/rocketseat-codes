c9348006ee8a8f405d4b896bc9fd6caf727a6feb
Alfredo Savi <alfredosavi@hotmail.com>
2021-06-24 10:45:42 -0300

    feat: Create table Compliment

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { AuthUserController } from "./controllers/AuthUserController";
+import { CreateComplimentController } from "./controllers/CreateComplimentController";
 
 import { ensureAdmin } from "./middlewares/ensureAdmin";
 
@@ -11,9 +12,11 @@ const router = Router();
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 const authUserController = new AuthUserController();
+const createComplimentController = new CreateComplimentController();
 
 router.post("/users", createUserController.create);
 router.post("/tags", ensureAdmin, createTagController.create);
 router.post("/session", authUserController.create);
+router.post("/compliments", createComplimentController.create);
 
 export { router };
