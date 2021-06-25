27d3d556419f9d26e4a482e6f73c245f13076948
Alfredo Savi <alfredosavi@hotmail.com>
2021-06-25 00:04:43 -0300

    feat: create question and admin

---
+++ src/hooks/useRoom.ts
@@ -0,0 +1,75 @@
+import { useState, useEffect } from "react";
+import { database } from "../services/firebase";
+import { useAuth } from "./useAuth";
+
+type FirebaseQuestions = Record<
+  string,
+  {
+    author: {
+      name: string;
+      avatar: string;
+    };
+    content: string;
+    isAnswered: boolean;
+    isHighLighted: boolean;
+    likes: Record<
+      string,
+      {
+        authorId: string;
+      }
+    >;
+  }
+>;
+
+type Questions = {
+  id: string;
+  author: {
+    name: string;
+    avatar: string;
+  };
+  content: string;
+  isAnswered: boolean;
+  isHighLighted: boolean;
+  likeCount: Number;
+  likeId: string | undefined;
+};
+
+export function useRoom(roomId: string) {
+  const { user } = useAuth();
+  const [questions, setQUestions] = useState<Questions[]>([]);
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    const roomRef = database.ref(`rooms/${roomId}`);
+
+    roomRef.on("value", (room) => {
+      const databaseRoom = room.val();
+      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
+
+      const parsedQuestions = Object.entries(firebaseQuestions).map(
+        ([key, value]) => {
+          return {
+            id: key,
+            content: value.content,
+            author: value.author,
+            isAnswered: value.isAnswered,
+            isHighLighted: value.isHighLighted,
+            likeCount: Object.values(value.likes ?? {}).length,
+            likeId: Object.entries(value.likes ?? {}).find(
+              ([key, like]) => like.authorId === user?.id
+            )?.[0],
+          };
+        }
+      );
+
+      setTitle(databaseRoom.title);
+      setQUestions(parsedQuestions);
+    });
+
+    return () => {
+      roomRef.off("value");
+    };
+  }, [roomId, user?.id]);
+
+  return { questions, title };
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,16 @@
 import { FormEvent } from "react";
 import { useHistory } from "react-router-dom";
-import { useAuth } from "../hooks/useAuth";
+import { useAuth } from "../../hooks/useAuth";
 
-import illustrationImg from "../assets/images/illustration.svg";
-import logoImg from "../assets/images/logo.svg";
-import googleIconImg from "../assets/images/google-icon.svg";
+import illustrationImg from "../../assets/images/illustration.svg";
+import logoImg from "../../assets/images/logo.svg";
+import googleIconImg from "../../assets/images/google-icon.svg";
 
-import { Button } from "../components/Button";
+import { Button } from "../../components/Button";
 
-import "../styles/auth.scss";
+import "../../styles/auth.scss";
 import { useState } from "react";
-import { database } from "../services/firebase";
+import { database } from "../../services/firebase";
 
 export function Home() {
   const { user, signInWithGoogle } = useAuth();
@@ -36,6 +36,11 @@ export function Home() {
 
     if (!roomRef.exists()) {
       alert("Room does not exists.");
+      return;
+    }
+
+    if (roomRef.val().endedAt) {
+      alert("Room already closed.");
       return;
     }
 
